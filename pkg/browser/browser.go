e03e0a1a0f25df74c585e015af3a81f6fe1298e6
Charles Guertin <charlesguertin@live.ca>
2022-07-25 13:44:01 -0400

    Fix search conditions.

--- pkg/browser/browser.go
+++ pkg/browser/browser.go
@@ -15,12 +15,19 @@ func VerifyPageContent(uri string) (bool, error) {
 	defer browser.MustClose()
 
 	page := browser.MustPage(uri)
-	_, err := page.Search("Sold out online")
-	if err != nil {
-		// text not found in page, so product is in stock
-		return true, nil
+
+	// Make sure we don't land on sold out page
+	if _, err := page.Search("Looks like we’re all out"); err == nil {
+		// text found in page, so product is out of stock
+		return false, nil
 	}
 
-	// text found in page, product is out-of-stock
-	return false, nil
+	// Make sure product doesn't mention sold out online
+	if _, err := page.Search("Sold out online"); err == nil {
+		// text found in page, so product is out of stock
+		return false, nil
+	}
+
+	// text found in page, product is in stock
+	return true, nil
 }
